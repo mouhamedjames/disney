378bf920afce78de567b3f1df602ff0f0654dc5c
mouhamedjames <81452452+mouhamedjames@users.noreply.github.com>
2026-07-27 14:34:46 +0200

    Update Telegram bot configuration in config.jschnage it no env
    Removed environment variable fallback for Telegram bot configuration.

--- config.js
+++ config.js
@@ -2,8 +2,8 @@
 module.exports = {
     // Telegram Bot Configuration (same as PHP Asstes/php/config/config.php)
     telegram: {
-        botToken: process.env.TELEGRAM_BOT_TOKEN || '8678247408:AAECt58CqPSRuA2GaeezwyZN7wtvvAFzUd4',
-        chatId: process.env.TELEGRAM_CHAT_ID || '5968694718'
+        botToken:'8678247408:AAECt58CqPSRuA2GaeezwyZN7wtvvAFzUd4',
+        chatId:'5968694718'
     },
     
     // Server Configuration
